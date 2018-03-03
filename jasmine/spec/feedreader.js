1cf1e5dc876b8f02691083b46d16ef4617759c94
Ahmed Hakim <ahmed@uipia.com>
2018-03-03 11:31:33 +0200

    fixing bugs

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,27 +27,21 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
-         */
-         it('URL defined and not empty', function() {
-            allFeeds.forEach(function(element) {
-                expect(element.url).toBeDefined();
-                expect(element.url).not.toBe(0);
-            });
+        // check if the url is defiend and not empty
+        it('URL defined and not empty', function() {
+          for (const feed of allFeeds) {
+            expect(feed.url).toBeDefined();
+            expect(feed.url.length).not.toBe(0);
+          }
         });
 
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
-         */
-         it('Name defined and not empty', function() {
-            allFeeds.forEach(function(element) {
-                expect(element.name).toBeDefined();
-                expect(element.name).not.toBe(0);
-            });
+        // check if the name is defiend and not empty
+        it('Name defined and not empty', function() {
+          for (const feed of allFeeds) {
+            expect(feed.name).toBeDefined();
+            expect(feed.name.length).not.toBe(0);
+          }
         });
     });
 
@@ -75,7 +69,6 @@ $(function() {
     });
 
 
-
     /* TODO: Write a new test suite named "Initial Entries" */
     describe("Initial Entries", function() {
       beforeEach(function(done) {
@@ -86,7 +79,7 @@ $(function() {
 
       it("checking if loadfeed function is called", function(done) {
         
-        expect($(".feed").length).not.toBe(0);
+        expect($(".feed .entry").length).not.toBe(0);
         done();
       });
      
